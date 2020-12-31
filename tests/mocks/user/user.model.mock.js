84520c747ff62ce4e6d4d5f707b0c730abb9735f
Santi Juarez <sjuareza11@gmail.com>
2020-12-31 18:52:23 +0100

    feat: (Project )  Migrate project from private repo
    NBA-1

---
+++ tests/mocks/user/user.model.mock.js
@@ -0,0 +1,22 @@
+module.exports = {
+  user: {
+    _id: "507f191e810c19729de860ea",
+    name: "Test",
+    username: "Test04",
+    password: "password123S",
+  },
+  users: [
+    {
+      _id: "507f191e810c19729de860ea",
+      name: "santiTest",
+      username: "santiTest12",
+      password: "prueba1234Aa",
+    },
+    {
+      _id: "507f191e810c19729de860eb",
+      name: "David",
+      username: "David2jj",
+      password: "usuario89",
+    },
+  ],
+};
